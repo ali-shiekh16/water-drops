7101b7a47ac4d12118718ceb392d6106161032fb
ali <ali.shiekh16@outlook.com>
2022-11-24 14:47:27 +0500

    Change base URL.

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,6 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), glsl()],
-  base: '/r3f-water-drops/',
   build: {
     chunkSizeWarningLimit: 1600,
   },
